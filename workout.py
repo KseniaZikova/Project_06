6e6d1320148ec6bec04ecefc1c2a18d2b77066c2
cenu_m <cenu_m@mail.ru>
2018-11-24 23:20:06 +0700

    Update workout.py

--- workout.py
+++ workout.py
@@ -4,16 +4,8 @@ def poisk_third_krit(first_krit, second_krit):
 """ Dividing line """
 def line_stolb(simvol):
     return simvol*64
-" Test "
-def ocenivanie(resalt):
-    if third_krit >= 90:
-        return resalt == 5
-    elif third_krit >= 70 and third_krit < 90:
-        return resalt == 4
-    elif third_krit < 70 and third_krit >= 50:
-        return resalt == 3
-    else:
-        return resalt == 2
+def line_stolb_1(simvol_1):
+    return simvol_1*44
 
 file_in = open("spisok.txt", 'r')
 line = file_in.read().splitlines()
@@ -30,13 +22,62 @@ first_krit = int(input(ru_local.SEMESTR))
 second_krit = int(input(ru_local.EXZAMEN))
 third_krit = poisk_third_krit(first_krit,second_krit)
 resalt = third_krit
-resalt = ocenivanie(third_krit)
+
+if third_krit >= 100:
+    resalt = ru_local.YES
+else:
+    resalt = ru_local.NO
+
 
 first_krit_1 = int(input(ru_local.SEMESTR))
 second_krit_1 = int(input(ru_local.EXZAMEN))
 third_krit_1 = poisk_third_krit(first_krit_1, second_krit_1)
 resalt_1 = third_krit_1
-resalt_1 = ocenivanie(third_krit_1)
+
+if third_krit_1 >= 100:
+    resalt_1 = ru_local.YES
+else:
+    resalt_1 = ru_local.NO
+
+first_krit_2 = int(input(ru_local.SEMESTR))
+second_krit_2 = int(input(ru_local.EXZAMEN))
+third_krit_2 = poisk_third_krit(first_krit_2, second_krit_2)
+resalt_2 = third_krit_2
+
+if third_krit_2 >= 100:
+    resalt_2 = ru_local.YES
+else:
+    resalt_2 = ru_local.NO
+
+first_krit_3 = int(input(ru_local.SEMESTR))
+second_krit_3 = int(input(ru_local.EXZAMEN))
+third_krit_3 = poisk_third_krit(first_krit_3, second_krit_3)
+resalt_3 = third_krit_3
+
+if third_krit_3 >= 100:
+    resalt_3 = ru_local.YES
+else:
+    resalt_3 = ru_local.NO
+
+first_krit_4 = int(input(ru_local.SEMESTR))
+second_krit_4 = int(input(ru_local.EXZAMEN))
+third_krit_4 = poisk_third_krit(first_krit_4, second_krit_4)
+resalt_4 = third_krit_4
+
+if third_krit_4 >= 100:
+    resalt_4 = ru_local.YES
+else:
+    resalt_4 = ru_local.NO
+
+first_krit_5 = int(input(ru_local.SEMESTR))
+second_krit_5 = int(input(ru_local.EXZAMEN))
+third_krit_5 = poisk_third_krit(first_krit_5, second_krit_5)
+resalt_5 = third_krit_5
+
+if third_krit_5 >= 100:
+    resalt_5 = ru_local.YES
+else:
+    resalt_5 = ru_local.NO
 
 print(line_stolb("-"))
 print('| {:^20} | {:^7} | {:^7} | {:^7} | {:^7} |'.format(ru_local.NAME, ru_local.ONE,
@@ -63,5 +104,34 @@ print(line_stolb("|"))
 print('| {:^20} | {:^7} | {:^7} | {:^7} | {:^7} |'.format(line_5, first_krit_4,
                                                           second_krit_4, third_krit_4,
                                                           resalt_4))
-
 print(line_stolb("-"))
+print()
+print(line_stolb_1("-"))
+print('{:^20} | {:^20}'.format(ru_local.SPISOK_1,ru_local.SPISOK_2))
+print(line_stolb_1("-"))
+if resalt == ru_local.YES:
+    print('{:^20} | {:^20}'.format(line_1,'--'))
+else:
+    print ('{:^20} | {:^20}'.format ('--', line_1))
+
+if resalt_1 == ru_local.YES:
+    print('{:^20} | {:^20}'.format(line_2,'--'))
+else:
+    print ('{:^20} | {:^20}'.format ('--', line_2))
+
+if resalt_2 == ru_local.YES:
+    print ('{:^20} | {:^20}'.format (line_3, '--'))
+else:
+    print ('{:^20} | {:^20}'.format ('--', line_3))
+
+if resalt_3 == ru_local.YES:
+    print ('{:^20} | {:^20}'.format (line_4, '--'))
+else:
+    print ('{:^20} | {:^20}'.format ('--', line_4))
+
+if resalt_4 == ru_local.YES:
+    print('{:^20} | {:^20}'.format(line_5,'--'))
+else:
+    print ('{:^20} | {:^20}'.format ('--', line_5))
+
+print (line_stolb_1("-"))
